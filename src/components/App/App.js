559ea90e980e034f34c72fa3d81f263f9dc7a983
Travis Lang <tlang50@icloud.com>
2018-12-03 14:26:02 -0600

    add GET route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,19 +3,40 @@ import axios from 'axios';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Select a Month</h1>
-          <h3>SELECTED MONTH GOES HERE</h3>
-          <br/>
-        </header>
-        <br/>
-        <p>List of months goes here</p>
-      </div>
-    );
-  }
+    state = {
+        months: []
+    }
+    
+    getMonths = () => {
+        axios.get('/calendar')
+        .then( res => {
+            this.setState({
+                months: res.data
+            })
+        }).catch( err => {
+            console.log( 'error in GET request:', err );
+        })
+    }
+
+    componentDidMount() {
+        this.getMonths();
+    }
+
+    render() {
+        console.log(this.state);
+        
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">Select a Month</h1>
+                    <h3>SELECTED MONTH GOES HERE</h3>
+                    <br />
+                </header>
+                <br />
+                <p>List of months goes here</p>
+            </div>
+        );
+    }
 }
 
 export default App;
